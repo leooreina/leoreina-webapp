4540a4e8dc10fc557e1ac952c1c1d6e2eb5e8088
Leonardo Reina <leonardo@formahomolog.com.br>
2020-05-04 23:02:42 -0300

    Adding first styles

--- src/core/App.js
+++ src/core/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Main from '../views/Main';
+import '../styles/App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      Hello world!
+      <Main/>
     </div>
   );
 }
---
+++ src/views/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from "react";
+
+export default class Main extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div></div>
+                <div>Leonardo Reina</div>
+            </React.Fragment>
+        )
+    }
+}
