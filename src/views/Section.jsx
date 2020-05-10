a734bd68e16c5b45f52e9e836079ba55b8e23836
Leonardo Reina <leonardo@formahomolog.com.br>
2020-05-09 21:58:57 -0300

    Applying style for menu in tablet and desktop

--- src/views/Section.jsx
+++ src/views/Section.jsx
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from "react";
+import '../styles/Section.scss';
 
 export default class Section extends Component {
     render() {
         return (
             <Fragment>
-                <div>
-                    <i className={this.props.icon}></i>
-                    <span>{this.props.label}</span>
+                <div className="background-section">
+                    <span className="section-icon"><i className={this.props.icon}></i></span>
+                    <span className="section-label">{this.props.label}</span>
                 </div>
             </Fragment>
         )
